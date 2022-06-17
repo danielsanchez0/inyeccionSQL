31de8c7cc7a8b3a60575bf7570042153e12db887
Daniel Sanchez <daniel991938@gmail.com>
2022-06-17 15:19:24 -0500

    inyeccion sql resuelta

--- login.php
+++ login.php
@@ -1,10 +1,12 @@
 <?php
 /* proceso de login, chequea si un usuario existe y el proceso es correcto */
 
-$email = $_POST['email'];
-$pass = $_POST['password'];
+//seguro para evitar SQL inyection
+$email =  mysqli_real_escape_string($conn,$_POST['email']);
+$pass =   mysqli_real_escape_string($conn,$_POST['password']);
 
 $sql = "SELECT * FROM users WHERE email= '$email' AND password = '$pass'";
+
 $result = mysqli_query($conn,$sql);
 
 
@@ -14,14 +16,21 @@ if ( $result->num_rows == 0 ){ // User doesn't exist
 }
 else {
     $user = $result->fetch_assoc();
-      
-    $_SESSION['email'] = $user['email'];
-    $_SESSION['first_name'] = $user['first_name'];
-    $_SESSION['last_name'] = $user['last_name'];
-    $_SESSION['active'] = $user['active'];
-        
-    // This is how we'll know the user is logged in
-    $_SESSION['logged_in'] = true;
 
-    header("location: profile.php");
+    if ( $_POST['password'] == $user['password']) {
+  
+        $_SESSION['email'] = $user['email'];
+        $_SESSION['first_name'] = $user['first_name'];
+        $_SESSION['last_name'] = $user['last_name'];
+        $_SESSION['active'] = $user['active'];
+            
+        // This is how we'll know the user is logged in
+        $_SESSION['logged_in'] = true;
+
+        header("location: profile.php");
+
+    } else{
+        $_SESSION['message'] = "Has ingresado una contraseña incorrecta!";
+        header("location: error.php");
+    }
 }
